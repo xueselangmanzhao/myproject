e19f2b6fc9d40a57b658d3ed4db260fa4659ce98
Bruce <1046244623@qq.com>
2025-03-18 00:55:19 +0800

    second commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index,tmp,yun
 
 urlpatterns = [
     path('', index, name='index'),
+    path('tmp/', tmp, name='tmp'),
+    path('yun/', yun, name='yun'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def tmp(request):
+    return render(request, 'tmp.html')
+
+def yun(request):
+    return render(request, 'yun.html')
