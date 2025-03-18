9b6ecbf3ea9b130732e5a5bdbf5276b396af98a5
Bruce <1046244623@qq.com>
2025-03-18 18:11:23 +0800

    d

--- myproject/settings.py
+++ myproject/settings.py
@@ -66,8 +66,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'myproject',
+        'USER': 'root',
+        'PASSWORD': '12345678',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
